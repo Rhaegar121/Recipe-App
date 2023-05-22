85fe964d077ade545c96c73fc8e5fa46d82ff630
Anthony Martin <anthonymartin20@gmail.com>
2023-05-22 05:17:08 -0400

    Add devise email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
 
   validates :name, presence: true, length: { minimum: 5, maximum: 50 }, allow_blank: false
 end
