24e8f192727b5fd98c05773a72fb6822140a82ed
Anthony Martin <anthonymartin20@gmail.com>
2023-05-22 08:37:48 -0400

    Add Recipes controller

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,40 @@
+class RecipesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @user = current_user
+    @recipe = @user.recipes.new(params[:recipe])
+
+    if @recipe.save
+      redirect_to recipes_path, notice: t('success')
+    else
+      render :new, alert: t('failure')
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path, notice: t('success')
+  rescue ActiveRecord::RecordNotFound
+    redirect_to recipes_path, alert: t('failure')
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
+end
