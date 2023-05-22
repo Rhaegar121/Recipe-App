4dcc7aa9ff322a4880ee7a3de16bb6652daba577
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-05-22 22:50:31 +0630

    add routes for general shopping list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :foods, only: [:index, :new, :create]
+  resources :foods, only: %i[index new create]
   resources :recipes, except: :edit
+  resources :general_shopping_list, only: %i[index]
 end
