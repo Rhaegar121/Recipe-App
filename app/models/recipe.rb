4590ce90711059b3e6f0bb56b949ef623adc9b10
Anthony Martin <anthonymartin20@gmail.com>
2023-05-22 09:54:28 -0400

    Render recipe options conditionally

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,12 @@ class Recipe < ApplicationRecord
   validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :description, presence: true, length: { minimum: 10, maximum: 500 }, allow_blank: false
   validates :public, inclusion: { in: [true, false] }
+
+  def show_description?(current_user)
+    public || user.admin? || user == current_user
+  end
+
+  def owner?(current_user)
+    user == current_user || user.admin?
+  end
 end
