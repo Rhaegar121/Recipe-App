ad759133f708a4d26845fe1b3bd91d94b06a0687
Anthony Martin <anthonymartin20@gmail.com>
2023-05-22 08:57:29 -0400

    Add link to recipe details

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module RecipeApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.i18n.availabe_locales = [:en]
     config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
