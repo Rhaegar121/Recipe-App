c6450f162b6c9643c92e66acca3ac3f64e59a6dc
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-05-22 23:14:25 +0630

    update view in shopping_lists/index.html.erb file

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,5 +1,16 @@
 class ShoppingListsController < ApplicationController
   def index
-    @recipes = Recipe.includes(recipe_foods: :food).where(public: true).order(:updated_at)
+    @user = current_user
+    @recipes = @user.recipes.includes(:foods) # Load associated foods for the recipes
+
+    # Get all the food IDs from the user's recipes
+    recipe_food_ids = @recipes.map { |recipe| recipe.foods.pluck(:id) }.flatten
+
+    # Find the missing foods by comparing the recipe food IDs with all the user's food IDs
+    @missing_foods = @user.foods.where.not(id: recipe_food_ids)
+
+    # Calculate the total food items and total price of the missing foods
+    @total_food_items = @missing_foods.sum(:quantity)
+    @total_price = @missing_foods.sum('quantity * price')
   end
 end
