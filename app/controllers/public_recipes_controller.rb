b81d07acd5f626d4cb4a1f35235a8f22f0ab403e
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-05-23 18:06:50 +0630

    update the view in public_recipes/index.html.erb file

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,19 @@
 class PublicRecipesController < ApplicationController
   def index
-    @recipes = Recipe.includes(:user).includes(:food).includes(:recipe_food).where(public: true)
+    @user = current_user
+    @recipes = @user.recipes.includes(:user).includes(:foods).where(public: true)
+
+    @recipes.each do |recipe|
+      recipe.recipe_foods.each { |recipe_food| recipe_food.quantity ||= 1 }
+    end
+
+    @total_food_items = @recipes.flat_map(&:recipe_foods).length
+    @total_price = 0
+
+    @recipes.each do |recipe|
+      recipe.recipe_foods.each do |recipe_food|
+        @total_price += recipe_food.food.price * recipe_food.quantity
+      end
+    end
   end
 end
