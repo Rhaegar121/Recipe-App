77de85ba5a2566da177827cb8720eed1db91ae3e
Anthony Martin <anthonymartin20@gmail.com>
2023-05-26 12:42:35 -0400

    Add letter_opener to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,13 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "RecipeApp_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false  
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
