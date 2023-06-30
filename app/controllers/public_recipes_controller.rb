ee66e167519cf8b70ed9f96ac0117387f49f8d9d
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-30 22:49:28 +0630

    change authentication page and add a splash screen

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,9 @@
 class PublicRecipesController < ApplicationController
+  before_action :authenticate_user!, except: [:splash]
+
   def index
     @recipes = Recipe.includes(:user, recipe_foods: [:food]).where(public: true).order('created_at DESC')
   end
+
+  def splash; end
 end
