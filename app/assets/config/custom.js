e20ae31f337d688c3730a44d496d1c38fadd1994
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-07-01 23:57:48 +0630

    add back button to header

--- app/assets/config/custom.js
+++ app/assets/config/custom.js
@@ -1,9 +1,14 @@
 document.addEventListener('turbo:load', function() {
   const hamburger = document.querySelector('.hamburger');
   const mobileMenu = document.querySelector('.nav-link');
+  const backbtn = document.querySelector('.back');
   
   hamburger.onclick = () => {
     hamburger.classList.toggle('open');
     mobileMenu.classList.toggle('height');
+  };
+
+  backbtn.onclick = () => {
+    window.history.back();
   };
 });
