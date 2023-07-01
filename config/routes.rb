6da1fa6844772b7b11ace84cec945b74f00dd67e
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-07-01 16:33:36 +0630

    update root path for authenticated user in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  authenticated :user do
+    root 'public_recipes#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'public_recipes#splash', as: :unauthenticated_root
+  end
+
   resources :foods, only: %i[index new create destroy]
   resources :recipes, except: :edit do
     resources :recipe_foods, only: %i[new create destroy]
   end
   resources :shopping_lists, only: %i[index]
   resources :public_recipes, only: %i[index]
-
-  root to: 'public_recipes#splash'
 end
